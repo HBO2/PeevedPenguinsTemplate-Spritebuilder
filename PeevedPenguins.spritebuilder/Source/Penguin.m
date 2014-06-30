c722fb31840b67410653e7e9acf9bc0506d4f9f0
HBO2 <bertvanhouten@icloud.com>
2014-06-30 12:23:08 +0200

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,13 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Bert on 30/06/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+@end
