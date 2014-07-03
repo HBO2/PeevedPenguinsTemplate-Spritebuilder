6deb3b29478e44ccc4ee2abf2a34171996396603
HBO2 <bertvanhouten@icloud.com>
2014-07-03 14:35:48 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -168,7 +168,6 @@
 
 
 - (void)sealRemoved:(CCNode *)seal {
-    [seal removeFromParent];
     // load particle effect
     CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
     // make the particle effect clean itself up, once it is completed
