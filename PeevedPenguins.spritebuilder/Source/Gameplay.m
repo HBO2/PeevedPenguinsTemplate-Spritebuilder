da27e044917c3ef0c049b52e6e6795aa94d502ff
HBO2 <bertvanhouten@icloud.com>
2014-07-03 12:12:05 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -36,6 +36,7 @@
     // nothing shall collide with our invisible nodes
     _pullbackNode.physicsBody.collisionMask = @[];
     _mouseJointNode.physicsBody.collisionMask = @[];
+    _physicsNode.collisionDelegate = self;
 }
 
 // called on every touch in this scene
